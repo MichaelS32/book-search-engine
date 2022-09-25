6004e3949256723b370234580f1ef232dc97baf9
mschneider <michaelschneider3232@gmail.com>
2022-09-25 09:38:48 -0500

    edited front end mutations and other files

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,54 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  cache: cache,
+  uri: 'http://localhost:3000/',
+  request: (operation) => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setConext({
+      headers: {
+        authorizatiojn: token ? `Bearer ${token}` : ""
+      }
+    });
+  },
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
+});
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
           <Routes>
-            <Route 
-              path='/' 
-              element={<SearchBooks />} 
+            <Route
+              path='/'
+              element={<SearchBooks />}
             />
-            <Route 
-              path='/saved' 
-              element={<SavedBooks />} 
+            <Route
+              path='/saved'
+              element={<SavedBooks />}
             />
-            <Route 
+            <Route
               path='*'
-              element={<h1 className='display-2'>Wrong page!</h1>} 
+              element={<h1 className='display-2'>Wrong page!</h1>}
             />
           </Routes>
-      </>
-    </Router>
+        </>
+      </Router>
+    </ApolloProvider>
   );
 }
 
